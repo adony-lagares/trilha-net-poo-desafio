ca9caa3e8574429c28c08e60e62b1afa9db70102
adony-lagares <adonyhibari48@gmail.com>
2023-09-14 15:30:01 -0300

    Desafio concluído.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone teste = new Iphone("31983418332", "onze", "123456", 64);
+teste.InstalarAplicativo("Whatsapp");
+
+Nokia testeNokia = new Nokia("31983324513", "tijolão", "654321", 128);
+testeNokia.InstalarAplicativo("Jogo da Cobrinha");
